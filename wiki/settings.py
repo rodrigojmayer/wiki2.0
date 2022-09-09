ef326ea47aedde3a245202f528c032e3daf957dd
Rodrigo <rodrigojmayer@gmail.com>
2022-09-09 15:30:50 +0200

    debug test

--- wiki/settings.py
+++ wiki/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', default='yoursecretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
+# DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
  
